dbf33fd6ad7b277618a5f31ead7221add8e74456
Sokunthaneth Chhoy <chhoysokunthaneth@gmail.com>
2019-06-29 21:19:34 +0700

    CRUD basic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  items: Item[] = [];
 
-  constructor() {}
+  newItem: Item = <Item>{};
 
+  @ViewChild('mylist')mylist: IonList;
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    })
+  }
+
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast('Item added!');
+      this.loadItems();
+    });
+  }
+
+  loadItems() {
+    this.storageService.getItems().then(items => {
+      this.items = items;
+    });
+  }
+
+  updateItem(item: Item) {
+    item.title = `UPDATED: ${item.title}`;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('Item updated!');
+      this.mylist.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
